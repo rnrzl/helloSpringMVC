5c63ebee98b25ebc9d3254db99725c8c4e46e52f
rnrzl <cooky0907@gmail.com>
2025-04-20 20:26:43 +0900

    add spring form functionality

--- src/main/java/kr/ac/hansung/cse/model/Offer.java
+++ src/main/java/kr/ac/hansung/cse/model/Offer.java
@@ -1,5 +1,6 @@
 package kr.ac.hansung.cse.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -13,7 +14,11 @@ import lombok.ToString;
 @Setter
 @ToString
 @NoArgsConstructor
+@Entity
+@Table(name="offers")
 public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Size(min=2, max=100, message = "Name must be between 2 and 100 chars")
